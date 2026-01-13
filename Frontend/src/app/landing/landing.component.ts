9001a5d624f89b720a06adcbdbe5ca78866a2038
KARTHIK <kv9423@gmail.com>
2026-01-13 11:29:59 +0530

    made more visibility and valid email and passwrd verified

--- Frontend/src/app/landing/landing.component.ts
+++ Frontend/src/app/landing/landing.component.ts
@@ -39,14 +39,28 @@ export class LandingComponent {
   authenticate() {
     this.errors = {};
 
-    if (this.authMode === 'signup' && !this.name) {
+    /* NAME (signup only) */
+    if (this.authMode === 'signup' && !this.name.trim()) {
       this.errors.name = 'Please enter your name';
     }
+
+    /* EMAIL VALIDATION */
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!this.email) {
       this.errors.email = 'Please enter your email';
+    } else if (!emailRegex.test(this.email)) {
+      this.errors.email = 'Please enter a valid email';
     }
+
+    /* PASSWORD VALIDATION */
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
     if (!this.password) {
       this.errors.password = 'Please enter your password';
+    } else if (!passwordRegex.test(this.password)) {
+      this.errors.password =
+        'The password must contain at least 1 uppercase, 1 lowercase, 1 special character, 1 number and length must be more than or equal to 8';
     }
 
     if (Object.keys(this.errors).length) return;
